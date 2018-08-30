82ba895b11e10c24ba24a9aa80a75732f42d27c7
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-08-30 12:16:06 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ cspp2-assignments/m4/Assignment-1/Solution.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Solution
+{/*
+	Fill this main function to print maximum of given array
+	*/
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		int maximum = 0; 
+		for (int i = 0; i<n; i++) {
+			arr[i] = sc.nextInt();
+			if (arr[i] > maximum) {
+				maximum = arr[i];
+			}
+		System.out.println(maximum);
+		}
+		
+
+	}
+}
