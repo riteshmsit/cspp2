d29cf45c2953afdec1b41f58923a73ff3ba464bb
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-08-30 15:16:50 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -26,25 +26,25 @@ final class Solution {
         int r2 = s.nextInt();
         int c2 = s.nextInt();
         int[][]b = new int [r2][c2];
-        for (int i = 0;i < r2; i++) {
-            for(int j = 0;j < c2;j++) {
+        for (int i = 0; i < r2; i++) {
+            for(int j = 0; j < c2; j++) {
                 b[i][j] = s.nextInt();
             }
         }
         int[][]c = new int[r2][c2];
-        for (int i = 0;i < r2;i++) {
-            for(int j = 0;j < c2;j++) {
+        for (int i = 0; i < r2; i++) {
+            for(int j = 0; j < c2; j++) {
                 c[i][j] = a[i][j] + b[i][j];
             }
         }
         if (r1 != r2 || c1 != c2) {
             System.out.println("not possible");
         }   else {
-            for (int i = 0;i < r2; i++) {
+            for (int i = 0; i < r2; i++) {
                 String st = "";
-                for(int j = 0;j < c2;j++) {
+                for(int j = 0; j < c2; j++) {
                     c[i][j] = a[i][j] + b[i][j];
-                    st += c[i][j] + " ";                
+                    st += c[i][j] + " ";            
             }
                 System.out.println(st.substring(0,st.length()-1));
         }
