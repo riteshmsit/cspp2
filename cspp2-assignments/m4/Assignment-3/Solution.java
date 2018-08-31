c66f49f018e76a015d6b95e1e0d985b8242a8666
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-08-31 16:56:55 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-3/Solution.java
+++ cspp2-assignments/m4/Assignment-3/Solution.java
@@ -1,7 +1,4 @@
-
-// Given an String, Write a java method that returns the decimal value for the given binary string.
-
-
+//Java program to convert binary to decimal
 import java.util.*;
 /**.
  * { class of the program }
@@ -9,40 +6,54 @@ import java.util.*;
 final class Solution
 {
 /*
-	Do not modify this main function.
-	*/
-	public static void main(final String[] args)
-	{
-		Scanner sc = new Scanner(System.in);
-		//sc.nextLine();
-		int n = Integer.parseInt(sc.nextLine());
-		for (int i = 0; i < n; i++) {
-			String s = sc.nextLine();
-			long res = binaryToDecimal(s);
-			System.out.println(res);
-		}
-				
-	}
-	public static long binaryToDecimal(String s) {
-		//int binary = 1;
-		//int decimal = 0;
-		//for (int i =s.length()-1;i>=0;i--) {
-		//if(s.charAt(i) == '1') {
-		//decimal +=binary;
-		//}
-		//binary = binary *2;
-		//}
-		//String res = Integer.toString(decimal);
-		//return res;
-	    long binary_value = Long.parseLong(s);
-	    long last_digit, i = 0;
-		long decimal_value = 0;
-		while (binary_value != 0) {
-			last_digit = binary_value % 10;
-			decimal_value += last_digit * Math.pow(2,i);
-			i += 1;
-			binary_value = binary_value / 10;
-		}
-		return decimal_value;	
-	}
+    Do not modify this main function.
+    */
+    /**.
+     * Constructs the object.
+     */
+    private Solution() {
+
+    }
+    public static void main(final String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        //sc.nextLine();
+        int n = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < n; i++) {
+            String s = sc.nextLine();
+            long res = binaryToDecimal(s);
+            System.out.println(res);
+        }         
+    }
+    /**.
+     * { function to convert binary to decimal}
+     *
+     * @param      s     { String input which contains number }
+     *
+     * @return     { returns decimal value }
+     */
+    public static long binaryToDecimal(final String s) {
+        //int binary = 1;
+        //int decimal = 0;
+        //for (int i =s.length()-1;i>=0;i--) {
+        //if(s.charAt(i) == '1') {
+        //decimal +=binary;
+        //}
+        //binary = binary *2;
+        //}
+        //String res = Integer.toString(decimal);
+        //return res;
+        long binary_value = Long.parseLong(s);
+        long last_digit, i = 0;
+        long decimal_value = 0;
+        while (binary_value != 0) {
+            last_digit = binary_value % 10;
+            decimal_value += last_digit * Math.pow(2,i);
+            i += 1;
+            binary_value = binary_value / 10;
+        }
+        return decimal_value;
+    }
 }
+
+
