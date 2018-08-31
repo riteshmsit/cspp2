9d73a01aa0f74fed228e5869ec46d9ea77917632
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-08-31 14:01:57 +0530

    Concatination

---
+++ cspp2-practice/m5/Concatinate.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Concatinate {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String sample_string = "hello ";
+		String input_string = s.nextLine();
+		String concat_string = sample_string.concat(input_string) + "!";
+		System.out.println(concat_string);
+	}
+}
