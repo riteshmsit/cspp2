fd44acfe2592fc3e1d8144b16cd0ea18d217eaad
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-09-01 09:51:20 +0530

    "check_style_score -> 1 of 6 passed. style score: 0/1 "

--- cspp2-assignments/m6/Assignment-1/Solution.java
+++ cspp2-assignments/m6/Assignment-1/Solution.java
@@ -17,7 +17,7 @@ public final class Solution {
      *
      * @param      n     n value
      */
-    static int oddComposites(final int n) {
+    static void oddComposites(final int n) {
 	// write your code here
     	for (int i = 3; i <= n; i += 2) {
     		int flag = 0;
@@ -26,11 +26,10 @@ public final class Solution {
     				flag = 1;
     			}
     		}
-    		if (flag == 0) {
-    			return i;
+    		if (flag == 1) {
+    			System.out.println(i);;
     		}
     	}
-    return 1;
     }
     /**
     * main method as driver program.
@@ -39,7 +38,7 @@ public final class Solution {
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
-        System.out.println(oddComposites(n));
+        oddComposites(n);
         
     }
 }
