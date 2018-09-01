d65adf91c7836a154a90d9873910d93f63202f77
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-09-01 10:03:04 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- cspp2-assignments/m6/Assignment-1/Solution.java
+++ cspp2-assignments/m6/Assignment-1/Solution.java
@@ -9,25 +9,28 @@ public final class Solution {
     /**
     * Empty constructor.
     */
+    public final static int T = 3; 
     private Solution() {
         //not used
     }
     /**
      * Prints the odd composite numbers between 2 and n.
      *
-     * @param      n     n value
+     * @param      n     input value
      */
     static void oddComposites(final int n) {
-	// write your code here
-    	for (long i = 3; i <= n; i += 2) {
-    		int flag = 0;
-    		for (int j = 2; j <= i/2; j += 1) {
-    			if (i % j == 0) {
-    				flag = 1;
-    			}
-    		}
-            if (flag == 1) System.out.println(i);
-    	}
+    // write your code here
+        for (long i = T; i <= n; i += 2) {
+            int flag = 0;
+            for (int j = 2; j <= i / 2; j += 1) {
+                if (i % j == 0) {
+                    flag = 1;
+                }
+            }
+            if (flag == 1) {
+                System.out.println(i);
+            }
+        }
     }
     /**
     * main method as driver program.
