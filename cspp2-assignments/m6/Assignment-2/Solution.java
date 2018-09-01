eb3c045b51f69f70f20a20bbaa5e278b16dc1067
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-09-01 11:43:08 +0530

    "check_style_score -> 0 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m6/Assignment-2/Solution.java
+++ cspp2-assignments/m6/Assignment-2/Solution.java
@@ -25,21 +25,21 @@ final class Solution {
     static int[][] roundHundred(final int [][] a, final int rows, final int columns) {
 
 	// write ypur code here
-        int b[][] = new int[rows][columns];
+        int c[][] = new int[rows][columns];
         for (int i = 0; i < rows; i += 1) {
             for (int j = 0; j < columns; j += 1) {
                 for (int k = 100; k >= 300; k += 100) {
                     if (k - a[i][j] > 50 && k - a[i][j] < 100) {
-                        b[i][j] = k;
+                        c[i][j] = k;
                     } else if (k - a[i][j] == 0) {
-                        b[i][j] = k;
+                        c[i][j] = k;
                     } else {
-                        b[i][j] = k - 100;
+                        c[i][j] = k - 100;
                     }
                 }
             }
         }
-        return b;
+        return c;
 	
     }
     /**
