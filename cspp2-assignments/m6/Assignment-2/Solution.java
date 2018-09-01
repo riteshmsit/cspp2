e71b38345e43b3d8738eb86b96ec4e84f3d3f369
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-09-01 12:36:03 +0530

    "check_style_score -> 0 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m6/Assignment-2/Solution.java
+++ cspp2-assignments/m6/Assignment-2/Solution.java
@@ -28,18 +28,7 @@ final class Solution {
         int [][] c = new int[rows][columns];
         for (int i = 0; i<rows; i++) {
             for(int j = 0; j<columns; j++) {
-                for (int k = 100; k >= 400; k += 100) {
-                    if (k - a[i][j] > 50 && k - a[i][j] < 100) {
-                        c[i][j] += 100;
-                        break;
-                    } if (k - a[i][j] == 0) {
-                        c[i][j] += k;
-                        break;
-                    } if (k - a[i][j] < 50 && k - a[i][j] < 100){
-                        c[i][j] += k;
-                        break;
-                    } 
-                }
+                c[i][j] = ((a[i][j] + 99) /100) * 100;
             }
         }
         return c;
