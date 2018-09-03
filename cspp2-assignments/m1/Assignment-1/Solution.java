fc2b284475c6c53fd064a20884749457e9bbe91f
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-09-03 21:05:04 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ cspp2-assignments/m1/Assignment-1/Solution.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Solution {
+	public static void main(String[] args) {
+		System.out.println("Name : , Roll Number : ");
+		Scanner sc = new Scanner(System.in);
+		String a = sc.nextLine();
+		int roll_number = sc.nextInt();
+		System.out.print(a);
+		System.out.print(roll_number);
+	}		
+	}
