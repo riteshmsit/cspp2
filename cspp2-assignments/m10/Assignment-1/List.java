0cc1733cf474b4a8261ae3fc397fcb7dda0b7290
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-09-06 19:22:52 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m10/Assignment-1/List.java
+++ cspp2-assignments/m10/Assignment-1/List.java
@@ -286,6 +286,11 @@ public class List {
     /**.
      * { item_description }
      */
+    /**.
+     * Adds all.
+     *
+     * @param      items  The items
+     */
     public void addAll(final int[] items) {
         // write the logic
         int i, j;
@@ -354,8 +359,7 @@ public class List {
                 if ((tokens.length) == 2) {
                 String[] t = tokens[1].split(",");
                 if (t.length == 1) {
-                    l.add(Integer.parseInt(tokens[1]));
-                }
+                    l.add(Integer.parseInt(tokens[1])); }
                 else {
                     if (t.length > 1) {
                         l.add(Integer.parseInt(t[0]), Integer.parseInt(t[1]));
@@ -369,7 +373,7 @@ public class List {
                 case "addAll":
                 if (tokens.length == 2) {
                 String[] t1 = tokens[1].split(",");
-                int temp[] = new int[t1.length];
+                int[] temp = new int[t1.length];
                 for (int i = 0; i < temp.length; i++) {
                     temp[i] = Integer.parseInt(t1[i]);
                 }
